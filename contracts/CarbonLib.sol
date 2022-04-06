ad2b222fa42b45d029629aedbfdacd5b1490e033
miguelfranken <miguel.franken@icloud.com>
2022-04-06 20:36:02 +0200

    Fix

--- contracts/CarbonLib.sol
+++ contracts/CarbonLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-library carbonLib {
+library CarbonLib {
     function getCid(uint id) external pure returns (string memory cid) {
         string memory batch;
 
